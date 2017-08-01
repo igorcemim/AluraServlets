b366cfc603ea64eef9783069f201d7051afa7122
Igor Cemim <igor@cemim.com.br>
2017-08-01 01:01:28 -0300

    Exercícios Alura.

---
+++ src/main/java/br/com/alura/gerenciador/web/FiltroAuditoria.java
@@ -0,0 +1,54 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Usuario;
+
+@WebFilter(urlPatterns="/*")
+public class FiltroAuditoria implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(
+			ServletRequest request,
+			ServletResponse response,
+			FilterChain chain)
+			throws IOException, ServletException {
+
+	    HttpServletRequest req = (HttpServletRequest) request;
+	    HttpServletResponse res = (HttpServletResponse) response;
+
+	    String usuario = getUsuario(req);
+	    System.out.println("Usuario " + usuario + " acessando a URI " + req.getRequestURI());
+
+	    chain.doFilter(request, response);
+	}
+	
+	private String getUsuario(HttpServletRequest req) {
+
+		Usuario usuario = (Usuario) req.getSession().getAttribute("usuario.logado");
+		if (usuario == null)
+			return "<deslogado>";
+		
+		return usuario.getEmail();
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
